badd5457b6f250c1a7588ef9bd5aa4e5d41c650e
mark82623 <4a0g0126@stust.edu.tw>
2017-05-21 11:56:32 +0800

    mark82623
    T fresh

--- demon/demon.cpp
+++ demon/demon.cpp
@@ -15,7 +15,7 @@ int main(int argc,char** argv)
 	Mat canny_output;
 
 	//讀取圖片
-	src = imread("D:\\ROC\\demon\\images\\sample.bmp",CV_LOAD_IMAGE_COLOR);
+	src = imread("D:\\OCR\\digital-recognize\\demon\\images\\sample.bmp",CV_LOAD_IMAGE_COLOR);
 	if(!src.data)
 	{
 		return -1;
@@ -25,22 +25,54 @@ int main(int argc,char** argv)
     cvtColor(src,src_gray,COLOR_BGR2GRAY);
 	imshow("Grayimage",src_gray);
 	
-	//放大
-	pyrUp(src_gray,src_gray,Size(src.cols*2,src.rows*2));
-	imshow("up",src_gray);
+	////放大
+	//pyrUp(src_gray,src_gray,Size(src.cols*2,src.rows*2));
+	//imshow("up",src_gray);
 
-	//高斯濾波
-	blur(src_gray,dst,Size(3,3));
-	imshow("blur",dst);
+	////高斯濾波
+	//blur(src_gray,dst,Size(3,3));
+	//imshow("blur",dst);
     
+	int T =0;
+	double Tmax;
+	double Tmin;;
+	minMaxIdx(src_gray,&Tmin,&Tmax);
+	T = ( Tmax + Tmin ) / 2;
+	while(true)
+	{
 
+		int Tosum =0,Tusum =0; //osum超過T加總 usum 小於T加總
+		int on = 0,un =0;  //on超過T的總數 un 小於T的總數 
+		for(int i = 0;i<src_gray.rows;i++)
+		{
+			for(int j = 0 ;j <src_gray.cols; j++)
+			{
+				if(src_gray.at<uchar>(i,j) >= T )
+				{
+					Tosum += src_gray.at<uchar>(i,j);
+					on ++;
+				}
+				else
+				{
+					Tusum += src_gray.at<uchar>(i,j);
+					un ++;
+				}
+			}
+		}
+		Tosum /=on;
+		Tusum /=un;
+		if((Tosum+Tusum) /2  != T)
+			T = (Tosum+Tusum) /2;
+		else
+			break;
+	}
 	//二值化
-	threshold(dst,dst,140,255,THRESH_BINARY);
-	//imshow("threshold",dst);
+	threshold(src_gray,dst,T,255,THRESH_BINARY);
+	imshow("threshold",dst);
 
 	//膨脹	
-	dilate(dst,dst,Mat(),Point(-1,-1),4);
-	imshow("1",dst);
+	/*dilate(dst,dst,Mat(),Point(-1,-1),1);
+	imshow("1",dst);*/
 	
 	int row = 1;
 	row = dst.rows;
@@ -112,9 +144,9 @@ int main(int argc,char** argv)
 	}
 	for(int i =0;i<n;i++)//篩選
 	{
-		if(sum[i] > 850)
+		if(sum[i] > 100)
 			sum[i] =0;
-		if(sum[i]< 320)
+		if(sum[i]< 25)
 			sum[i] = 0;
 	}
 	for(int i = 0;i< dst.rows;i++) // 清除篩選結果
@@ -135,27 +167,27 @@ int main(int argc,char** argv)
 	Canny(dst,dst,0,50,5);  //建立輪廓Canny
 	imshow("canny",dst);
 	
-	vector<vector<Point>> approx;  //尋找輪廓
-	findContours(dst,approx,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+	//vector<vector<Point>> approx;  //尋找輪廓
+	//findContours(dst,approx,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
 
-	int xx = 0;
-	for(size_t i = 0;i<approx.size()-1;i++)  //計算平均座標
-	{
-		Point bb = approx[i][0];
-		xx += bb.x;
-	}
-    xx /= approx.size();
-	for(size_t i = 1;i<approx.size()-1;i++)
-	{
-		Point bb = approx[i][0];
-		if(abs(approx[i][0].x - approx[i-1][0].x) >10 && abs(xx - bb.x) < 60 )  //條件一 不要重複 條件二 離數字太遠
-		{
-			Mat roi = src_gray(Rect(bb.x-5,bb.y-5,25,40));
-			char buffer[3];
-			sprintf(buffer,"%d",i);
-			imshow(buffer,roi);
-		}
-	}
+	//int xx = 0;
+	//for(size_t i = 0;i<approx.size()-1;i++)  //計算平均座標
+	//{
+	//	Point bb = approx[i][0];
+	//	xx += bb.x;
+	//}
+ //   xx /= approx.size();
+	//for(size_t i = 1;i<approx.size()-1;i++)
+	//{
+	//	Point bb = approx[i][0];
+	//	if(abs(approx[i][0].x - approx[i-1][0].x) >10 && abs(xx - bb.x) < 60 )  //條件一 不要重複 條件二 離數字太遠
+	//	{
+	//		Mat roi = src_gray(Rect(bb.x-5,bb.y-5,25,40));
+	//		char buffer[3];
+	//		sprintf(buffer,"%d",i);
+	//		imshow(buffer,roi);
+	//	}
+	//}
 
 
 	waitKey(0);

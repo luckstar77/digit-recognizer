dab3060ff4ced9eaa5454d22167eec0a62cf5d39
luckstar77y <luckstar77y@gmail.com>
2017-06-06 17:04:17 +0800

    fix the ALDigitRecognize divide function

---
+++ demon/ALDigitRecognize.hpp
@@ -0,0 +1,14 @@
+//
+//  ALDigitRecognize.hpp
+//  shintao-recognize
+//
+//  Created by develop on 2017/6/6.
+//  Copyright © 2017年 develop. All rights reserved.
+//
+
+#ifndef ALDigitRecognize_hpp
+#define ALDigitRecognize_hpp
+
+unsigned char *ALDigitRecognize(unsigned char, unsigned char *, char *);
+
+#endif /* ALDigitRecognize_hpp */

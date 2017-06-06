ab3a3f728af3b2786479bcdb8bdb6c0a400ed49d
luckstar77y <luckstar77y@gmail.com>
2017-06-06 23:49:34 +0800

    remove num folder

--- demon/num/3/path2.bat
+++
@@ -1,6 +0,0 @@
-setlocal enabledelayedexpansion
-for /f "delims=  " %%a in (num.txt) do (
-set /a line =3
-echo %%a >>0.txt
-echo !line! >>0.txt
-)
--- demon/num/4/path2.bat
+++
@@ -1,6 +0,0 @@
-setlocal enabledelayedexpansion
-for /f "delims=  " %%a in (num.txt) do (
-set /a line =4
-echo %%a >>0.txt
-echo !line! >>0.txt
-)
